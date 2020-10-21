22aa168d103b456a642fb23229e2a8f4f6f3dbb5
Vinicius Alves <vinicius.alves.contato@gmail.com>
2020-10-21 20:46:03 -0300

    Added section 34 assignment

---
+++ section-34/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-at:click="callback">
+                    Trigger
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            callback: function(){
+                alert('Triggered')
+            },
+        },
+        
+        directives: {
+            at: {
+                bind: function(el, binding){
+                    console.log('Entered bind')
+                    if (binding.arg === 'click') {
+                        console.log(binding.value)
+                        el.addEventListener('click', binding.value)
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
