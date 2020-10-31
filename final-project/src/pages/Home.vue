a8242051c399d8ca3f6b6351a2ba5bd3e92ff0f4
Vinicius Alves <vinicius.alves.contato@gmail.com>
2020-10-30 21:56:33 -0300

    Finished home page layout

--- final-project/src/pages/Home.vue
+++ final-project/src/pages/Home.vue
@@ -1,9 +1,27 @@
 <template>
-    <v-container>
+    <v-card class="pa-8 mt-4">
         <v-row>
-            Home
+            <span class="text-h2">
+                Trade or View your Portfolio
+            </span>
         </v-row>
-    </v-container>
+        <v-row class="pt-6">
+            <span class="text-h6">
+                You may Save & Load your Data
+            </span>
+        </v-row>
+        <v-row class="py-6">
+            <span class="text-h6">
+                Click on 'End Day' to begin a new Day!
+            </span>
+        </v-row>
+        <v-divider class="pt-4"></v-divider>
+        <v-row class="pt-4">
+            <span class="text-h5">
+                Your Funds: $10.000
+            </span>
+        </v-row>
+    </v-card>
 </template>
 
 <script>
