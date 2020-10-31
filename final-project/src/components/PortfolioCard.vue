8210b70f81c4ed1433a395f5d1b2f8ac79cfb2de
Vinicius Alves <vinicius.alves.contato@gmail.com>
2020-10-30 22:43:05 -0300

    created portfolio card and portfolio layout

---
+++ final-project/src/components/PortfolioCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <BaseStockCard :stockName="stockName" :coloringClasses="'blue blue--text'">
+        <template v-slot:price>
+            (Price: 110 | Quantity: 100)
+        </template>
+        <template v-slot:button>
+            <v-col cols="2">
+                <v-btn color="red">Sell</v-btn>
+            </v-col>
+        </template>
+    </BaseStockCard>
+</template>
+
+<script>
+    import BaseStockCard from './base/BaseStockCard.vue'
+    export default {
+        props: {
+            stockName: {
+                required: true,
+                type: String
+            }
+        },
+        components: {
+            BaseStockCard
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- final-project/src/pages/Portfolio.vue
+++ final-project/src/pages/Portfolio.vue
@@ -1,14 +1,18 @@
 <template>
     <v-container>
         <v-row>
-            Portfolio
+            <PortfolioCard stockName="BMW"></PortfolioCard>
+            <PortfolioCard stockName="Google"></PortfolioCard>            
         </v-row>
     </v-container>
 </template>
 
 <script>
+    import PortfolioCard from '../components/PortfolioCard.vue'
     export default {
-
+        components: {
+            PortfolioCard
+        }
     }
 </script>
 
